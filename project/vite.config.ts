004769f8acec08320b43b621972532a41a931228
Jos77we <josemaina207@gmail.com>
2024-12-16 22:44:51 +0300

    changes made

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
+// Replicate `__dirname` in ESM
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 export default defineConfig({
   plugins: [react()],
@@ -10,5 +14,6 @@ export default defineConfig({
     outDir: 'dist', // Explicitly set the output directory
     emptyOutDir: true, // Clean the output directory before building
   },
+  // Configure the public directory to include root files like `_headers` and `_redirects`
+  publicDir: path.resolve(__dirname),
 });
-
